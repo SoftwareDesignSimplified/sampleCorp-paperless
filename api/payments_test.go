b1e89df4148928cc7d1a150f6dbc8e3f1c749463
John Gallagher <john@synapticmishap.co.uk>
2023-10-09 14:55:13 +0100

    test: cleaner

--- api/payments_test.go
+++ api/payments_test.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
 	mockdb "github.com/qwetu_petro/backend/db/mock"
 	db "github.com/qwetu_petro/backend/db/sqlc"
@@ -13,66 +12,64 @@ import (
 	"io"
 	"net"
 	"net/http"
-	"net/http/httptest"
 	"reflect"
 	"testing"
 	"time"
 )
 
-func TestPaymentImproved(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	store := mockdb.NewMockStore(ctrl)
-
-	paymentRequest := db.PaymentRequest{
-		Amount:           123.0,
-		Description:      "wow",
-		Currency:         "USD",
-		AmountInWords:    "One hundred",
-		PaymentRequestNo: "1234",
-		RequestID:        1234,
-	}
-	// Set up your mock store expectations
+func TestPaymentApprovalAPIApprovedForAnyUser(t *testing.T) {
+	store, srv := setupTestServerAndMockStore(t)
+	store.EXPECT().GetUserByUserNameOrEmail(gomock.Any(), gomock.Any()).Return(db.User{}, nil)
+	store.EXPECT().GetUserRoles(gomock.Any(), gomock.Any()).Return([]db.UserRole{}, nil)
+	paymentRequest := createRandomPaymentRequest()
 	store.EXPECT().ApprovePaymentRequestWithAudit(gomock.Any(), gomock.Any(), gomock.Any()).Return(paymentRequest, nil)
 
-	r := gin.Default()
-	server := newTestServer(t, store)
-	r.POST("/payment-request/approve", func(c *gin.Context) {
-		userId := int64(1234)
-		c.Set("user_id", userId)
-		server.approvePaymentRequest(c)
-	})
-
-	body := `{ "amount": "123", "description": "wow", "currency": "USD", "amount_in_words": "One hundred" }`
-	req, err := http.NewRequest(http.MethodPost, "/payment-request/approve", bytes.NewBufferString(body))
-	require.NoError(t, err)
-
-	w := httptest.NewRecorder()
-
-	r.ServeHTTP(w, req)
-
-	require.Equal(t, http.StatusOK, w.Code)
-
-	var expectedPaymentRequest db.PaymentRequest
-	err = json.Unmarshal(w.Body.Bytes(), &expectedPaymentRequest)
+	response, err := makePaymentRequestApprovalRequest(t, srv)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("Could not make request: %v", err)
 	}
 
-	require.Equal(t, paymentRequest, expectedPaymentRequest)
+	require.Equal(t, http.StatusOK, response.StatusCode)
+
+	bodyBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response body: %v", err)
+	}
+	var paymentRequestFromResponse db.PaymentRequest
+	err = json.Unmarshal(bodyBytes, &paymentRequestFromResponse)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+
+	requireEqualPaymentRequestsByName(t, paymentRequest, paymentRequestFromResponse)
 	require.NoError(t, err)
 }
 
-func TestEndToEnd(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+func makePaymentRequestApprovalRequest(t *testing.T, srv *Server) (*http.Response, error) {
+	request, err := http.NewRequest(
+		"POST",
+		"http://localhost:8085/payment-request/approve",
+		bytes.NewBufferString(`{ "amount": "123", "description": "Description", "currency": "USD", "amount_in_words": "One hundred" }`),
+	)
+	if err != nil {
+		return nil, err
+	}
 
-	store := mockdb.NewMockStore(ctrl)
-	srv := newTestServer(t, store)
-	store.EXPECT().GetUserByUserNameOrEmail(gomock.Any(), gomock.Any()).Return(db.User{}, nil)
-	store.EXPECT().GetUserRoles(gomock.Any(), gomock.Any()).Return([]db.UserRole{}, nil)
+	token, _, err := srv.tokenMaker.CreateToken("username", 300000000)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Authorization", "Bearer "+token)
 
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		t.Fatalf("Could not make request: %v", err)
+	}
+	return response, err
+}
+
+func createRandomPaymentRequest() db.PaymentRequest {
 	adminID := int32(1234)
 	approvalDate := time.Now()
 	invoiceID := int32(1234)
@@ -93,7 +90,15 @@ func TestEndToEnd(t *testing.T) {
 		InvoiceID:        &invoiceID,
 		PdfUrl:           &pdfUrl,
 	}
-	store.EXPECT().ApprovePaymentRequestWithAudit(gomock.Any(), gomock.Any(), gomock.Any()).Return(paymentRequest, nil)
+	return paymentRequest
+}
+
+func setupTestServerAndMockStore(t *testing.T) (*mockdb.MockStore, *Server) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := mockdb.NewMockStore(ctrl)
+	srv := newTestServer(t, store)
 
 	go func() {
 		if err := srv.Start("0.0.0.0:8085"); !errors.Is(err, http.ErrServerClosed) {
@@ -102,43 +107,7 @@ func TestEndToEnd(t *testing.T) {
 		}
 	}()
 
-	request, err := http.NewRequest(
-		"POST",
-		"http://localhost:8085/payment-request/approve",
-		bytes.NewBufferString(`{ "amount": "123", "description": "Description", "currency": "USD", "amount_in_words": "One hundred" }`),
-	)
-	if err != nil {
-		return
-	}
-
-	token, _, err := srv.tokenMaker.CreateToken("username", 300000000)
-	if err != nil {
-		return
-	}
-	request.Header.Set("Authorization", "Bearer "+token)
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		t.Fatalf("Could not make request: %v", err)
-	}
-
-	if response.StatusCode != 200 {
-		t.Fatalf("Expected status 200, got %v", response.StatusCode)
-	}
-
-	bodyBytes, err := io.ReadAll(response.Body)
-	if err != nil {
-		t.Fatalf("Failed to read response body: %v", err)
-	}
-	var paymentRequestFromResponse db.PaymentRequest
-	err = json.Unmarshal(bodyBytes, &paymentRequestFromResponse)
-	if err != nil {
-		t.Fatalf("Failed to unmarshal JSON: %v", err)
-	}
-
-	requireEqualPaymentRequestsByName(t, paymentRequest, paymentRequestFromResponse)
-	require.NoError(t, err)
+	return store, srv
 }
 
 func requireEqualPaymentRequestsByName(t *testing.T, expected, actual db.PaymentRequest) {
